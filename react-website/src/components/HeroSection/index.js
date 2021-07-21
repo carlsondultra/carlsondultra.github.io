69079f2441d255c1dafb8b4424ef713f5ab5d344
carlsondultra <carlsondultra@users.noreply.github.com>
2021-07-21 18:23:05 -0400

    Button, text added

--- react-website/src/components/HeroSection/index.js
+++ react-website/src/components/HeroSection/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Video from '../../videos/video.mp4';
-import { HeroContainer, HeroBg, VideoBg} from './HeroElements';
+import {Button} from '../ButtonElements';
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements';
 
 const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    };
+
     return (
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' /> 
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Carlson Dultra</HeroH1>
+                <HeroP>Bottom Text</HeroP>
+                <HeroBtnWrapper>
+                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'> Button {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     );
 };
