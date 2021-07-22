85d89dfa953440845c0d8c4e30b04f4d7bc1407e
carlsondultra <carlsondultra@users.noreply.github.com>
2021-07-21 21:17:16 -0400

    Added multiple sections into website

--- react-website/src/components/InfoSection/Data.js
+++ react-website/src/components/InfoSection/Data.js
@@ -7,7 +7,41 @@ export const homeObjOne = {
     headLine: 'ANother ttest',
     description: 'smallest test text',
     buttonLabel: 'Inside Button',
-    imgStart: false,
+    imgStart: false, /*changing which side image is on, left or right*/
+    img: require('../../images/svg1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'about', /*about section*/
+    lightBg: false, 
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Test C',
+    headLine: 'ANother ttest',
+    description: 'smallest test text',
+    buttonLabel: 'Inside Button',
+    imgStart: false, /*changing which side image is on, left or right*/
+    img: require('../../images/svg1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjThree = {
+    id: 'about', /*about section*/
+    lightBg: false, 
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Test C',
+    headLine: 'ANother ttest',
+    description: 'smallest test text',
+    buttonLabel: 'Inside Button',
+    imgStart: false, /*changing which side image is on, left or right*/
     img: require('../../images/svg1.svg'),
     alt: 'Car',
     dark: true,
