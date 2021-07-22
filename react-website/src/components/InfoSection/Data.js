a83a257861fa8ac29466590cf802b976e4d3a1df
carlsondultra <carlsondultra@users.noreply.github.com>
2021-07-21 21:09:26 -0400

    Finished adding second section to website

---
+++ react-website/src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about', /*about section*/
+    lightBg: false, 
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Test C',
+    headLine: 'ANother ttest',
+    description: 'smallest test text',
+    buttonLabel: 'Inside Button',
+    imgStart: false,
+    img: require('../../images/svg1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
