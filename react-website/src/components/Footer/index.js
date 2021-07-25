5719fc2181a56d6d74bf59a9b67bd0b351164277
carlsondultra <carlsondultra@users.noreply.github.com>
2021-07-25 16:27:00 -0400

    Fixing footer, adding proper links

--- react-website/src/components/Footer/index.js
+++ react-website/src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { FaEnvelope, FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
 import { animateScroll as scroll } from 'react-scroll'
 import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia,
 SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink } from './FooterElements'
@@ -13,60 +13,19 @@ const Footer = () => {
     return (
         <FooterContainer>
             <FooterWrap>
-                <FooterLinksContainer>
-                    <FooterLinksWrapper>
-                        <FooterLinkItems>
-                            <FooterLinkTitle> This is Footer </FooterLinkTitle>
-                                <FooterLink to="/signin">This is footer number 2</FooterLink>
-                                <FooterLink to="/signin">This is footer number 3</FooterLink>
-                                <FooterLink to="/signin">This is footer number 4</FooterLink>
-                                <FooterLink to="/signin">This is footer number 5</FooterLink>
-                                <FooterLink to="/signin">This is footer number 6</FooterLink>
-                        </FooterLinkItems>
-                        <FooterLinkItems>
-                            <FooterLinkTitle> This is Footer </FooterLinkTitle>
-                                <FooterLink to="/signin">This is footer number 2</FooterLink>
-                                <FooterLink to="/signin">This is footer number 3</FooterLink>
-                                <FooterLink to="/signin">This is footer number 4</FooterLink>
-                                <FooterLink to="/signin">This is footer number 5</FooterLink>
-                                <FooterLink to="/signin">This is footer number 6</FooterLink>
-                        </FooterLinkItems>
-                    </FooterLinksWrapper>
-                    <FooterLinksWrapper>
-                        <FooterLinkItems>
-                            <FooterLinkTitle> This is Footer </FooterLinkTitle>
-                                <FooterLink to="/signin">This is footer number 2</FooterLink>
-                                <FooterLink to="/signin">This is footer number 3</FooterLink>
-                                <FooterLink to="/signin">This is footer number 4</FooterLink>
-                                <FooterLink to="/signin">This is footer number 5</FooterLink>
-                                <FooterLink to="/signin">This is footer number 6</FooterLink>
-                        </FooterLinkItems>
-                        <FooterLinkItems>
-                            <FooterLinkTitle> This is Footer </FooterLinkTitle>
-                                <FooterLink to="/signin">This is footer number 2</FooterLink>
-                                <FooterLink to="/signin">This is footer number 3</FooterLink>
-                                <FooterLink to="/signin">This is footer number 4</FooterLink>
-                                <FooterLink to="/signin">This is footer number 5</FooterLink>
-                                <FooterLink to="/signin">This is footer number 6</FooterLink>
-                        </FooterLinkItems>
-                    </FooterLinksWrapper>
-                </FooterLinksContainer>
                 <SocialMedia>
                     <SocialMediaWrap>
-                        <SocialLogo to='/'onClick={toggleHome}>Logo</SocialLogo>
+                        <SocialLogo to='/'onClick={toggleHome}>Carlson Dultra</SocialLogo>
                         <WebsiteRights>Carlson Dultra © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
                         <SocialIcons>
-                            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
-                                <FaFacebook />
+                            <SocialIconLink href="//www.linkedin.com/in/carlsondultra/" target="_blank" aria-label="LinkedIn">
+                                <FaLinkedin />
                             </SocialIconLink>
-                            <SocialIconLink href="/" target="_blank" aria-label="Instagram">
-                                <FaInstagram />
+                            <SocialIconLink href="//www.github.com/carlsondultra" target="_blank" aria-label="Github">
+                                <FaGithub />
                             </SocialIconLink>
-                            <SocialIconLink href="//www.youtube.com" target="_blank" aria-label="Youtube">
-                                <FaYoutube />
-                            </SocialIconLink>
-                            <SocialIconLink href="/" target="_blank" aria-label="Twitter">
-                                <FaTwitter />
+                            <SocialIconLink href="mailto:carlson.dultra@ryerson.ca" target="_blank" aria-label="Email">
+                                <FaEnvelope />
                             </SocialIconLink>
                         </SocialIcons>
                     </SocialMediaWrap>
