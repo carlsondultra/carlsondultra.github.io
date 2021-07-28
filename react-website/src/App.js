8a17a073c73f4c08cb78d16ccc183b39f3a86979
carlsondultra <carlsondultra@users.noreply.github.com>
2021-07-27 21:29:47 -0400

    Deploying

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from './pages';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {HashRouter as Router, Switch, Route} from 'react-router-dom'
 import SigninPage from './pages/signin';
 /*import Navbar from './components/Navbar';*/
 
